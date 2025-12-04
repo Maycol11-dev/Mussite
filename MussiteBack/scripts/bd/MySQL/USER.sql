62466c1de64bfe2bd56e9c3304ffae7e24277d3d
Maycol11-dev <maycol.alconz.et12@gmail.com>
2025-12-04 20:03:55 -0300

    Delete MussiteBack directory

--- MussiteBack/scripts/bd/MySQL/USER.sql
+++
@@ -1,6 +0,0 @@
-
-DROP USER IF EXISTS 'admin'@'localhost';
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'Admin123!';
-
-GRANT SELECT,INSERT,UPDATE,DELETE ON bd_MussiteDatabase.* TO 'admin'@'localhost';
-
